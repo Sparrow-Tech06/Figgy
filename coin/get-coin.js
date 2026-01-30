fcde1185ee9633d301c3f33f6810a3eef1ce70f3
Sparrow-Tech06 <cpal980075@gmail.com>
2026-01-30 13:35:48 +0530

    Update get-coin.js

--- coin/get-coin.js
+++ coin/get-coin.js
@@ -1,5 +1,5 @@
 function getCoin(amount = 0){
-  let wallet = parseInt(localStorage.getItem("myCoins")) || 0;
-  wallet += amount;
-  localStorage.setItem("myCoins", wallet);
+  let coins = parseInt(localStorage.getItem("myCoins")) || 0;
+  coins += amount;
+  localStorage.setItem("myCoins", coins);
 }
