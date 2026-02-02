14f1744f272c82152165e4a5058f6ae5868908cf
Sparrow-Tech06 <cpal980075@gmail.com>
2026-02-02 08:59:13 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
       flex:1;
       display:flex;
       flex-direction:column;
-      justify-content:space-around;
+      justify-content:space-evenly;
       align-items:center;
       gap:16px;
     }
